a484be962861a16921919bc4d4e0c2e3a7c191d7
Luvuyo Tafeni <luvuyotafeni012@gmail.com>
2026-02-02 15:36:54 +0200

    email service for approval initialization

--- usermanagementservice/src/main/java/com/banking/usermanagementservice/service/serviceImpl/EmailServiceImpl.java
+++ usermanagementservice/src/main/java/com/banking/usermanagementservice/service/serviceImpl/EmailServiceImpl.java
@@ -1,11 +1,34 @@
 package com.banking.usermanagementservice.service.serviceImpl;
 
 import com.banking.usermanagementservice.service.EmailService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class EmailServiceImpl implements EmailService {
+
+    private  final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     @Override
     public void sendApprovalEmail(String toEmail, String userName, String otp) {
+        log.info("sending approval email to: {}", toEmail);
 
+        String subject = "Your banking account had been approved!";
+        String body = buildApprovalEmailBody(userName,otp);
+
+        sendEmail(toEmail, subject, body);
     }
 
     @Override
@@ -21,5 +44,35 @@ public class EmailServiceImpl implements EmailService {
     @Override
     public void sendPasswordExpirationWarning(String toEmail, String userName, int daysRemaining) {
 
+    }
+
+    private void sendEmail(String to, String subject, String body){
+        try{
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setFrom(fromEmail);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            mailSender.send(message);
+            log.info("Email sent successfully to: {}", to);
+        } catch (MessagingException e) {
+            log.error("Failed to send email to: {}", to, e);
+            throw new RuntimeException("Failed to send email", e);
+        }
+    }
+
+    private String buildApprovalEmailBody(String userName, String otp){
+        return String.format("""
+                <!DOCTYPE html>
+                <html>
+                <head>
+                </head>
+                <body>
+                </body>
+                </html>
+                """, userName, otp
+        );
     }
 }
