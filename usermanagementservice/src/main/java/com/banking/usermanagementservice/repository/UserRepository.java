89736455d5bb29196ff6630f1fd8ad98de8721c4
Luvuyo Tafeni <luvuyotafeni012@gmail.com>
2026-02-02 13:28:31 +0200

    userRepository layer addition with it's queries

---
+++ usermanagementservice/src/main/java/com/banking/usermanagementservice/repository/UserRepository.java
@@ -0,0 +1,36 @@
+package com.banking.usermanagementservice.repository;
+
+import com.banking.usermanagementservice.entity.User;
+import com.banking.usermanagementservice.enums.ApprovalStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByIdNumber(String idNumber);
+    boolean existsByEmail(String email);
+    boolean existsByIdNumber(String idNumber);
+
+    @Query("SELECT u FROM User u WHERE u.isDeleted = false")
+    List<User> findAllActive();
+
+    @Query("SELECT u FROM User u WHERE u.approvalStatus = :status AND u.isDeleted = false")
+    List<User> findByApprovalStatus(@Param("status")ApprovalStatus status);
+
+    @Query("SELECT u FROM User u WHERE u.id = :id AND u.isDeleted = false")
+    Optional<User> findByIdAndNotDeleted(@Param("id") UUID id);
+
+    @Query("SELECT u FROM User u WHERE u.email = :email AND u.isDeleted = false")
+    Optional<User> findByEmailAndNotDeleted(@Param("email") String email);
+
+    @Query("SELECT u FROM User u JOIN u.roles WHERE r.name = :roleName AND u.isDeleted = false")
+    Optional<User> findByRoleName(@Param("roleName") String roleName);
+}
