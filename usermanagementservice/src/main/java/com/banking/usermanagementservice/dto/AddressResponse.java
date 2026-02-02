21743870a7dddbef1c05fb8281516e7ba9f0f0dd
Luvuyo Tafeni <luvuyotafeni012@gmail.com>
2026-02-02 13:14:15 +0200

    Address response addition

--- usermanagementservice/src/main/java/com/banking/usermanagementservice/dto/AddressResponse.java
+++ usermanagementservice/src/main/java/com/banking/usermanagementservice/dto/AddressResponse.java
@@ -1,4 +1,27 @@
 package com.banking.usermanagementservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AddressResponse {
+
+    private UUID id;
+    private String streetAddress;
+    private String streetAddress2;
+    private String suburb;
+    private String city;
+    private String province;
+    private String postalCode;
+    private String country;
+    private String formattedAddress;
 }
