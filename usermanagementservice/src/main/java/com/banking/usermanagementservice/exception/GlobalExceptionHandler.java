fe96ca530d2aacb6bb4424633db2b000bf437dff
Luvuyo Tafeni <luvuyotafeni012@gmail.com>
2026-02-02 20:54:49 +0200

    Exception handling class successfully added

--- usermanagementservice/src/main/java/com/banking/usermanagementservice/exception/GlobalExceptionHandler.java
+++ usermanagementservice/src/main/java/com/banking/usermanagementservice/exception/GlobalExceptionHandler.java
@@ -1,14 +1,74 @@
 package com.banking.usermanagementservice.exception;
 
+import com.banking.usermanagementservice.dto.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiResponse>
+    public ResponseEntity<ApiResponse<Void>> handleResourceNotFoundException(ResourceNotFoundException ex){
+        log.error("Resource not found: {}", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+
+    @ExceptionHandler(DuplicateResourceException.class)
+    public  ResponseEntity<ApiResponse<Void>> handleDuplicateResourceException(DuplicateResourceException ex){
+        log.error("Duplicate resource: {}", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+
+    @ExceptionHandler(InvalidOperationException.class)
+    public  ResponseEntity<ApiResponse<Void>> handleInvalidOperationException(InvalidOperationException ex){
+        log.error("Invalid operation: {}", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public  ResponseEntity<ApiResponse<Map<String, String>>> handleValidationExceptions(MethodArgumentNotValidException ex){
+        Map<String, String > errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error)->{
+            String  fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        log.error("Validation failed: {}", errors);
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.<Map<String , String>>builder()
+                        .success(false)
+                        .message("Validation failed")
+                        .data(errors)
+                        .build()
+                );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleGeneralException(Exception ex){
+        log.error("Unexpected error occured", ex);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error("An unexpected error occured", ex.getMessage()));
+    }
+
+
+
 }
