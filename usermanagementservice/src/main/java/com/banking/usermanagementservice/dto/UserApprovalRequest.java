358ef0b2092f2591ffa8126e8cb4022eaf162e15
Luvuyo Tafeni <luvuyotafeni012@gmail.com>
2026-02-02 13:04:29 +0200

    User approval dto addition

---
+++ usermanagementservice/src/main/java/com/banking/usermanagementservice/dto/UserApprovalRequest.java
@@ -0,0 +1,26 @@
+package com.banking.usermanagementservice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserApprovalRequest {
+
+    @NotNull(message = "User Id is required")
+    private UUID userId;
+
+    @NotNull(message = "Approval decision is required")
+    private boolean approved;
+
+    @NotBlank(message = "Reason is required")
+    private String reason;
+}
