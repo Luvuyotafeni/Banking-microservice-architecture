994203a369ded6c9490a16be190e97121c7f1ea6
Luvuyo Tafeni <luvuyotafeni012@gmail.com>
2026-02-02 20:38:27 +0200

    Api Response code

---
+++ usermanagementservice/src/main/java/com/banking/usermanagementservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.banking.usermanagementservice.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse>
+}
