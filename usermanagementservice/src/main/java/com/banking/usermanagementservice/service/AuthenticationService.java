74d93dd3c4a4bbdb227839e0f01320893d23f7ee
Luvuyo Tafeni <luvuyotafeni012@gmail.com>
2026-02-03 08:20:06 +0200

    Authentication service interface (methods) addition

---
+++ usermanagementservice/src/main/java/com/banking/usermanagementservice/service/AuthenticationService.java
@@ -0,0 +1,4 @@
+package com.banking.usermanagementservice.service;
+
+public interface AuthenticationService {
+}
---
+++ usermanagementservice/src/main/java/com/banking/usermanagementservice/service/serviceImpl/AuthenticationServiceImpl.java
@@ -0,0 +1,4 @@
+package com.banking.usermanagementservice.service.serviceImpl;
+
+public class AuthenticationServiceImpl {
+}
